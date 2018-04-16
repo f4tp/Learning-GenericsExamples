2803c66092bae2da3b882852316b254d02f2f28e
f4tp <mr_treadwell@live.co.uk>
2018-04-16 15:28:59 +0100

    Initial Commit to publish

---
+++ GenericsExample/List.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsExample
+{
+    class List
+    {
+
+        public void Add(Object objectin)
+        {
+
+        }
+
+        public int this[int index]
+        {
+            get { throw new NotImplementedException(); }
+        }
+    }
+}
